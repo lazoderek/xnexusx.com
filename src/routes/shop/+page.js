e261a8610fbe6ba1b276ca34e28381192b1cda9b
Derek <lazo.derek@gmail.com>
2023-05-29 16:57:04 -0700

    +page.js consolidation

--- src/routes/shop/+page.js
+++ src/routes/shop/+page.js
@@ -1,12 +1,66 @@
-import { getProducts } from '../../../store.js';
-export const load = async (ctx) => {
-    await getProducts();
-    const productType = ctx.page.query.get('type');
-    if (productType) {
-        products.update((items) => {
-            const updated = items.filter((product) => product.node.productType === productType);
-            return updated;
-        });
+export const load = async (LoadEvent) => {
+
+    const query = `{
+        products(sortKey: TITLE, first: 10) {
+            edges {
+                node {
+                    id
+                    handle
+                    description
+                    title
+                    totalInventory
+                    productType
+                    variants(first: 5) {
+                        edges {
+                            node {
+                                id
+                                title
+                                price
+                                inventoryQuantity
+                        }
+                    }
+                }
+                priceRangeV2 {
+                    maxVariantPrice {
+                        amount
+                        currencyCode
+                    }
+                    minVariantPrice {
+                        amount
+                        currencyCode
+                    }
+                }
+                images(first: 1) {
+                    edges {
+                        node {
+                            url
+                            altText
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }`
+
+    try {
+        const result = await fetch(import.meta.env.VITE_SHOPIFY_API_ENDPOINT, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Shopify-Storefront-Access-Token': import.meta.env.VITE_SHOPIFY_STOREFRONT_API_TOKEN
+            },
+            body: JSON.stringify({ query })
+        }).then((res) => res.json());
+        if (result.errors) {
+            console.log({ errors: result.errors });
+        } else if (!result || !result.data) {
+            console.log({ result });
+            return 'No results found.';
+        }
+        console.log(result.data);
+        return result.data;
+    } catch (error) {
+        console.log(error);
     }
-    return { props: { products} };
 }
